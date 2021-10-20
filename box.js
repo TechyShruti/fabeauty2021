a1a163d0568083e2c5960183baffb682a8a2c99d
shandilya1689 <86498452+shandilya1689@users.noreply.github.com>
2021-10-20 08:29:54 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,56 @@
+function myimage1()
+{
+	document.getElementById("gall1").src="gall5.jpg";
+	
+	
+}
+
+function myimage2()
+{
+	document.getElementById("gall2").src="gall6.jpg";
+	
+	
+}
+
+function myimage3()
+{
+	document.getElementById("gall3").src="gall7.jpg";
+	
+	
+}
+
+function myimage4()
+{
+	document.getElementById("gall4").src="gall8.jpg";
+	
+	
+}
+
+
+function myimage5()
+{
+	document.getElementById("gall5").src="gall1.jpg";
+	
+	
+}
+
+function myimage6()
+{
+	document.getElementById("gall6").src="gall2.jpg";
+	
+	
+}
+
+function myimage7()
+{
+	document.getElementById("gall7").src="gall3.jpg";
+	
+	
+}
+
+function myimage8()
+{
+	document.getElementById("gall8").src="gall4.jpg";
+	
+	
+}
